1f69492a69546a90f25822da2331e095cf3cc3ac
Ibrahim <ibrahim_ay@live.de>
2014-12-26 14:45:33 +0100

    revert commit

--- bin/www.js
+++ bin/www.js
@@ -5,6 +5,6 @@ var app = require('../app');
 app.set('port', process.env.PORT || 8082);
 
 var server = app.listen(app.get('port'), function() {
-  console.log("ariba ariba");
+  console.log("ariba");
   debug('Express server listening on port ' + server.address().port);
 });
