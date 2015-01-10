a16c6d3d91f71bddf78327a9ad722777d7bd9873
Nabil Nasri <n.nabil@live.de>
2015-01-10 22:03:40 +0100

    controller.js wird nun erkannt

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,12 +1,1 @@
-function handleOrientation(event) {
-    var x = event.beta;  // In degree in the range [-180,180]
-    var y = event.gamma; // In degree in the range [-90,90]
-
-    console.log(y, " GAMMA");
-    console.log(x, "BETA");
-}
-
-alert("HU");
-
-
-window.addEventListener('deviceorientation', handleOrientation, true);
+alert("yo");
