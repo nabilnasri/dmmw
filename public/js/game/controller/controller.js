ecc7b515d02e541865d44f774ec1335397b96d2f
Paul Truschkin <paul.truschkin@hotmail.de>
2015-01-12 16:04:23 +0100

    bissel was geändert, orderstruktur und so

--- public/js/game/controller/controller.js
+++ public/js/game/controller/controller.js
@@ -1,67 +1,62 @@
-$(document).ready(function() {
-    alert("yo");
+function moveIt(ev) {
+    var orion = window.screen.orientation.type;
 
-    function moveIt(ev) {
-        var orion = window.screen.orientation.type;
+    if (orion === "landscape-primary") {
+        landscape_primary(ev);
 
-        if (orion === "landscape-primary") {
-            landscape_primary(ev);
+    } else if (orion === "landscape-secondary") {
+        landscape_secondary(ev);
 
-        } else if (orion === "landscape-secondary") {
-            landscape_secondary(ev);
+    } else if (orion === "portrait-primary") {
+        portrait_primary(ev);
+    }
+}
 
-        } else if (orion === "portrait-primary") {
-            portrait_primary(ev);
-        }
+function landscape_primary(ev) {
+    var acc = ev.accelerationIncludingGravity;
+
+    if (acc.y < -1) {
+        return "left";
+    }
+    else if
+    (acc.y > 1) {
+        return "right";
+    } else {
+        return "stop";
     }
 
-    function landscape_primary(ev) {
-        var acc = ev.accelerationIncludingGravity;
+}
 
-        if (acc.y < -1) {
-            return "left";
-        }
-        else if
-        (acc.y > 1) {
-            return "right";
-        } else {
-            return "stop";
-        }
 
+function landscape_secondary(ev) {
+    var acc = ev.accelerationIncludingGravity;
+
+    if (acc.y > 1) {
+        return "left";
+    }
+    else if
+    (acc.y < -1) {
+        return "right";
+    } else {
+        return "stop";
     }
 
+}
 
-    function landscape_secondary(ev) {
-        var acc = ev.accelerationIncludingGravity;
 
-        if (acc.y > 1) {
-            return "left";
-        }
-        else if
-        (acc.y < -1) {
-            return "right";
-        } else {
-            return "stop";
-        }
+function portrait_primary(ev) {
+    var acc = ev.accelerationIncludingGravity;
 
+    if (acc.x > 1) {
+        return "left";
+    }
+    else if
+    (acc.x < -1) {
+        return "right";
+    } else {
+        return "stop";
     }
 
+}
 
-    function portrait_primary(ev) {
-        var acc = ev.accelerationIncludingGravity;
-
-        if (acc.x > 1) {
-            return "left";
-        }
-        else if
-        (acc.x < -1) {
-            return "right";
-        } else {
-            return "stop";
-        }
-
-    }
-
-    window.addEventListener('devicemotion', moveIt, false);
-
-});
+window.addEventListener('devicemotion', moveIt, false);
